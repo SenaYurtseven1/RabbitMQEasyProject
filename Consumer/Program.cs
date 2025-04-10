d17ed39e2c2cf2dd16b527d51768f95d8dfeea9e
Sena <Sena@DESKTOP-GC0VQKG>
2025-04-10 15:15:26 +0300

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,29 @@
+﻿//Create a connection to the RabbitMQ server
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory connectionFactory = new();
+connectionFactory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+//Activate the connection and open a channel
+using IConnection connection = await connectionFactory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+//Create a queue
+await channel.QueueDeclareAsync(queue: "example-queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+//Read a message
+AsyncEventingBasicConsumer consumer = new(channel);
+await channel.BasicConsumeAsync(queue: "example-queue", autoAck: true, consumer: consumer);
+consumer.ReceivedAsync += async (sender, ea) =>
+{
+    //The place that the message is received and processed
+    //e.Body: the all arguments in message that is being processed
+    //e.Body.Span or e.Body.ToArray(): the byte message that is being processed in
+    byte[] body = ea.Body.ToArray();
+    string message = System.Text.Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Received message: {message}");
+};
+
+
+Console.Read();
---
+++ Publisher/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//Create a connection to the RabbitMQ server
+ConnectionFactory connectionFactory = new();
+connectionFactory .Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+//Activate the connection and open a channel
+using IConnection connection = await connectionFactory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+//Create a queue
+await channel.QueueDeclareAsync(queue: "example-queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+//Sending a message
+//byte[] message = Encoding.UTF8.GetBytes("Hello");
+//await channel.BasicPublishAsync(exchange:"",routingKey: "example-queue", body: message);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes("Hello " + i);
+    await channel.BasicPublishAsync(exchange:"",routingKey: "example-queue", body: message);
+
+}
+
+Console.Read();
